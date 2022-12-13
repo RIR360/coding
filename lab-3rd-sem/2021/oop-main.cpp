54cf8cf9a536517833c3e473a82822fa397e90c2
Rejwan Islam Rizvy <50569315+RIR360@users.noreply.github.com>
2022-12-13 20:41:51 +0600

    Update oop-main.cpp

--- lab-3rd-sem/2021/oop-main.cpp
+++ lab-3rd-sem/2021/oop-main.cpp
@@ -1,1 +1,58 @@
 // write your code with question name and output
+
+// Create a class float that contains one float data number. Over load all the four arithmetic operators for two objects (lab-6)
+#include <iostream>
+using namespace std;
+
+class Float {
+    
+    float data;
+    public:
+    Float(float d = 0) {data = d;}
+    Float operator + (Float f) {
+        Float temp;
+        temp.data = data + f.data;
+        return temp;
+    }
+    Float operator- (Float f) {
+        Float temp;
+        temp.data = data - f.data;
+        return temp;
+    }
+    Float operator* (Float f) {
+        Float temp;
+        temp.data = data * f.data;
+        return temp;
+    }
+    Float operator/ (Float f) {
+        Float temp;
+        temp.data = data / f.data;
+        return temp;
+    }
+    float display() {
+        return data;
+    }
+};
+
+int main()
+{
+    Float f1 = Float(2.5);
+    Float f2 = Float(1.3);
+    Float f3;
+    
+    f3 = f1 + f2;
+    cout << "Addition: " << f3.display() << endl;
+    
+    f3 = f1 - f2;
+    cout << "Subtraction: " << f3.display() << endl;
+    
+    f3 = f1 * f2;
+    cout << "Multiplication: " << f3.display() << endl;
+    
+    f3 = f1 / f2;
+    cout << "Division: " << f3.display() << endl;
+    
+    return 0;
+}
+
+// lab-6 end
