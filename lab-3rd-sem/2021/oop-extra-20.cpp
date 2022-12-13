52eb56e23a5cb28904bac4790318cdbd4c16bf67
Rejwan Islam Rizvy <50569315+RIR360@users.noreply.github.com>
2022-12-13 22:32:12 +0600

    Update oop-extra-20.cpp

--- lab-3rd-sem/2021/oop-extra-20.cpp
+++ lab-3rd-sem/2021/oop-extra-20.cpp
@@ -265,4 +265,229 @@ Case Swapped: AMARpassword
 --------------------------------------
 */
 
+/*
+(oop-extra-6) - Rizvy
+Question: Write a c++ program that swaps the case of string input taken from user
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  string str;
+  int n, i;
+  
+  cout << "Enter a string: ";
+  cin >> str;
+  
+  n = 0;
+  while(str[n] != '\0') n++;
+  
+  cout << "Case Swapped: ";
+  
+  for (i = 0; i < n; i++) {
+    
+    int letter = int(str[i]);
+    if (letter >= 97)
+      cout << char(letter - 32);
+    else 
+      cout << char(letter + 32);
+    
+  }
+  
+
+  return 0;
+}
+
+
+/*
+Output:
+--------------------------------------
+Enter a string: amarPASSWORD
+Case Swapped: AMARpassword
+--------------------------------------
+*/
+
+/*
+(oop-extra-7) - Rahin
+Question: Write a C++ program to check if number is positive, negative or zero.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter a number: ";
+    cin>> num;
+
+    if (num>0){
+        cout<<"The number is positive.\n";
+    }
+    else if(num<0){
+        cout<<"The number is negative.\n";
+    }
+    else{
+        cout<<"The number is Zero.\n";
+    }
+    return 0;
+}
+
+
+/*
+Output:
+--------------------------------------
+Enter a number: 6
+The number is positive.
+
+Enter a number: 5
+The number is negative.
+
+Enter a number: 0
+The number is zero.
+--------------------------------------
+*/
+
+/*
+(oop-extra-8) - Rahin
+Question: Write a C++ program to check whether a character is alphabet, digit or special character.
+*/
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    char c;
+    cout<< "Enter a character: ";
+    cin>>c;
+    
+    if((int(c)>=97 && int(c)<=122)||(int(c)>=65 && int(c)<=90)){
+        cout<<"The character is an Alphabet";
+    }
+    else if((int(c)>=48 && int(c)<=57)){
+        cout<<"The character is a Digit";
+    }
+    else{
+        cout<<"The character is a Special character";
+    }
+    cout<<endl;
+ 
+    return 0;
+}
+
+
+/*
+Output:
+--------------------------------------
+Enter a character: 1
+The character is a Digit
+
+Enter a character: a
+The character is an Alphabet
+
+Enter a character: $
+The character is a Special character
+--------------------------------------
+*/
+
+/*
+(oop-extra-9) - Rahin
+Question: Write a C++ program to calculate the total marks, percentage and division of student.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int oop, ds, ode, fbs, ca, total;
+    float percentage, division;
+    cout<<"Enter the marks of Object Oriented programming: ";
+    cin>>oop;
+    
+    cout<<"Enter the marks of Data Structure: ";
+    cin>>ds;
+    
+    cout<<"Enter the marks of Ordinary Differential Equation: ";
+    cin>>ode;
+    
+    cout<<"Enter the marks of Fundamental Of Business Studies: ";
+    cin>>fbs;
+    
+    cout<<"Enter the marks of Computer Architecture: ";
+    cin>>ca;
+    
+    total = oop + ds + ode + fbs + ca;
+    percentage=total/500.0*100;
+    
+    cout<<"The total marks: "<<total<<endl;
+    cout<<"The percentage : "<<percentage<<" %"<<endl;
+    division = (4*total)/500.0;
+    
+    if(division>=3.00)
+        cout<<"The Division   : 1st Division"<<endl;
+    else if(division>=2.25 && division<=2.99)
+        cout<<"The Division   : 2nd Division"<<endl;
+    else if(division>=2.00 && division<=2.24)
+        cout<<"The Division   : 3nd Division"<<endl;
+    else cout<<"The Division   : Fail"<<endl;
+  
+    return 0;
+}
+
+/*
+Output:
+------------------------------------------------------------
+Enter the marks of Object Oriented programming: 89
+Enter the marks of Data Structure: 89
+Enter the marks of Ordinary Differential Equation: 86
+Enter the marks of Fundamental Of Business Studies: 90
+Enter the marks of Computer Architecture: 88
+The total marks: 442
+The percentage :88.4 %
+The Division   : 1st Division
+------------------------------------------------------------
+*/
+
+
+/*
+(oop-extra-10) - Rahin
+Question: Write a C++ program to check whether a number is palindrome or not.
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int len, i, j;
+    string str;
+    
+    cout<<"Enter a string: ";
+    cin>>str;
+    
+    len=str.length();
+    char strRev[len];
+    for(i=0, j=len-1;i<len;i++,j--){
+        strRev[i]=str[j];
+    }
+    
+    for(i=0;i<len;i++){
+        if(strRev[i]!=str[i]){
+            cout<<"The string is not a phalindrome!";
+            return 0;
+        }
+    }
+    cout<<"The string is a phalindrome!";
+    return 0;
+}
+
+/*
+Output:
+--------------------------------------
+Enter a string: hello
+The string is not a phalindrome!
+
+Enter a string: moon
+The string is a phalindrome!
+--------------------------------------
+*/
 
