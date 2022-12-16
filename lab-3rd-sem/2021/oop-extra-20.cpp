25b72f0882de3052ab62fb0fb4bae0e071b6d6ed
Rejwan Islam Rizvy <50569315+RIR360@users.noreply.github.com>
2022-12-16 09:49:41 +0600

    Update oop-extra-20.cpp

--- lab-3rd-sem/2021/oop-extra-20.cpp
+++ lab-3rd-sem/2021/oop-extra-20.cpp
@@ -327,20 +327,17 @@ int main()
     cout<< "Enter a character: ";
     cin>>c;
     
-    if((int(c)>=97 && int(c)<=122)||(int(c)>=65 && int(c)<=90)){
-        cout<<"The character is an Alphabet";
-    }
-    else if((int(c)>=48 && int(c)<=57)){
-        cout<<"The character is a Digit";
-    }
-    else{
-        cout<<"The character is a Special character";
-    }
-    cout<<endl;
+    int a = int(c);
+    
+    if((a>=97 && a<=122) || (a>=65 && a<=90)) 
+        cout<<"The character is an Alphabet\n";
+    else if((a>=48 && a<=57))
+        cout<<"The character is a Digit\n";
+    else
+        cout<<"The character is a Special character\n";
  
     return 0;
 }
-
 
 /*
 Output:
